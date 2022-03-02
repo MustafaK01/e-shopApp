9b1e55941e0335afe0e6da3c466a24b50ee65d60
Mustafa Köroğlu <62333156+MustafaK01@users.noreply.github.com>
2022-03-02 20:31:51 +0300

    Added routes
    Added routes for cart operations in shop component and for shop component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+//import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { ModelModule } from './model/model.module';
+import { CartDetailsComponent } from './shop/cart-details/cart-details.component';
+import { CheckOutComponent } from './shop/check-out/check-out.component';
 import { ShopModule } from './shop/shop.module';
+import { ShopComponent } from './shop/shop/shop.component';
 
 
 @NgModule({
@@ -15,7 +20,13 @@ import { ShopModule } from './shop/shop.module';
   ],
   imports: [
     BrowserModule,
-    ShopModule
+    ShopModule,
+    RouterModule.forRoot([ 
+    { path:'shop',component:ShopComponent },
+    { path:'cart',component:CartDetailsComponent },
+    { path:'checkout',component:CheckOutComponent },
+    { path:'**',redirectTo:'shop'}
+  ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -4,14 +4,23 @@ import { BrowserModule } from "@angular/platform-browser";
 import { ModelModule } from "../model/model.module";
 import { ShopComponent } from './shop/shop.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CartSummaryComponent } from './cart-summary/cart-summary.component';
+import { CartDetailsComponent } from './cart-details/cart-details.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { RouterModule } from "@angular/router";
 @NgModule({
-    imports:[ModelModule,BrowserModule,FormsModule],
+    imports:[ModelModule,BrowserModule,FormsModule,RouterModule],
     declarations: [
       ShopComponent,
-      NavbarComponent
+      NavbarComponent,
+      CartSummaryComponent,
+      CartDetailsComponent,
+      CheckOutComponent
     ],
     exports: [
       ShopComponent,
+      CartDetailsComponent,
+      CheckOutComponent
      ]
 })
 export class ShopModule{
