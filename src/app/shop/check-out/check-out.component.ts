b06de304749eeaebce1c9126742db20baa01ef6a
Mustafa Köroğlu <62333156+MustafaK01@users.noreply.github.com>
2022-03-03 04:44:00 +0300

    Add for order form

--- src/app/shop/check-out/check-out.component.ts
+++ src/app/shop/check-out/check-out.component.ts
@@ -1,16 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Order } from 'src/app/model/order.model';
+import { OrderRepository } from 'src/app/model/order.repository';
+import { AlertifyService } from 'src/app/utils/alertify.service';
+import { OrderFormChecker } from 'src/app/utils/OrderFormChecker';
 
 @Component({
   selector: 'shop-check-out',
   templateUrl: './check-out.component.html',
-  styleUrls: ['./check-out.component.css']
+  styleUrls: ['./check-out.component.css'],
+  providers:[AlertifyService,OrderFormChecker]
 })
 export class CheckOutComponent implements OnInit {
-
-  constructor() { }
+  
+  isOrderSent:boolean=false;
+  isSubmitted:boolean=false;
+  alertify:AlertifyService;
+  orderFormChecker:OrderFormChecker;
+  private orderRepository:OrderRepository;
+  public order:Order;
+  constructor(orderRepository:OrderRepository,order:Order,
+    orderFormChecker:OrderFormChecker,alertify:AlertifyService) {
+    this.orderRepository=orderRepository;
+    this.order=order;
+    this.orderFormChecker=orderFormChecker;
+    this.alertify=alertify;
+   }
 
   ngOnInit(): void {
   }
+  formSubmit(form:NgForm){
+    this.isSubmitted=true;
 
+    if(this.orderFormChecker.checkIfAllEntered(form.value.name,form.value.email,
+      form.value.address,form.value.phone,
+      form.value.city)){
+        if(this.orderFormChecker.checkIfTheEmailIsEntered(form.value.email)){
+          if(form.valid){
+            this.orderRepository.saveOrder(this.order).subscribe(data=>{
+              this.order.clearOrder();
+              this.isOrderSent=true;
+              this.isSubmitted=false;
+            })
+            this.alertify.success("Siparişiniz Alındı")
+        }
+      }else{
+        this.alertify.error("Lütfen E-Mail Adresinizi Doğru Giriniz")
+      }
+      }else{
+        this.alertify.error("Lütfen Bİlgilerinizi Giriniz")
+      }
+    }
 }
 
