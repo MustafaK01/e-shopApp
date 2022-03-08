55a09754f5ea91d854e2eed5fe9b056849304165
Mustafa Köroğlu <62333156+MustafaK01@users.noreply.github.com>
2022-03-09 00:05:59 +0300

    Update

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -3,10 +3,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
 import { AuthGuard } from './auth.guard';
 import { AuthComponent } from './auth/auth.component';
+import { CategoriesFormComponent } from './categories/categories-form/categories-form.component';
+import { CategoriesListComponent } from './categories/categories-list/categories-list.component';
+import { OrderListComponent } from './orders/order-list/order-list.component';
+import { ProductsFormComponent } from './products/products-form/products-form.component';
+import { ProductsListComponent } from './products/products-list/products-list.component';
 
 const routes: Routes = [
   {path:'auth',component:AuthComponent},
-  {path:'main',component:AdminComponent,canActivate:[AuthGuard]},
+  {path:'main',component:AdminComponent,canActivate:[AuthGuard],children:[
+    {path:'products',component:ProductsListComponent},
+    {path:'products/:mode/:id',component:ProductsFormComponent},
+    {path:'products/:mode/',component:ProductsFormComponent},
+    {path:'categories/:mode/:id/',component:CategoriesListComponent},
+    {path:'categories/:mode/',component:CategoriesFormComponent},
+    {path:'categories',component:CategoriesListComponent},
+    {path:'orders',component:OrderListComponent},
+
+  ]},
   {path:'**',redirectTo:'auth'},
   {path:'admin',redirectTo:'auth'}];
 
