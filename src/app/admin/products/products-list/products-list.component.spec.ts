e94f6cc157ef2a479ed607a6c0d8cf1f26cfc534
Mustafa Köroğlu <62333156+MustafaK01@users.noreply.github.com>
2022-03-08 23:59:23 +0300

    Update
    Product-list component name has changed.
    Products-list component has been edited, the admin can see the products list after logging in

--- src/app/admin/products/products-list/products-list.component.spec.ts
+++ src/app/admin/products/products-list/products-list.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ProductListComponent } from './product-list.component';
+import { ProductsListComponent } from './products-list.component';
 
 describe('ProductListComponent', () => {
-  let component: ProductListComponent;
-  let fixture: ComponentFixture<ProductListComponent>;
+  let component: ProductsListComponent;
+  let fixture: ComponentFixture<ProductsListComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProductListComponent ]
+      declarations: [ ProductsListComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ProductListComponent);
+    fixture = TestBed.createComponent(ProductsListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
